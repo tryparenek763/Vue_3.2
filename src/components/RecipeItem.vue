dfbe6e9706f47d7384f4ad35d2a29664e08f50bd
Maxi <minzdrav1631@gmail.com>
2022-02-09 19:20:18 +0400

    Добавил стартовую верстку. Подготовка проекта

---
+++ src/components/RecipeItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>recipe item</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <RecipeList> </RecipeList>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-});
+<script setup lang="ts">
+import RecipeList from "../components/RecipeList.vue";
 </script>
